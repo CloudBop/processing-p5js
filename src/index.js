25140935611dee692e3b163c57c7029f3f5ba50b
Cloudbop <boppincolin@gmail.com>
2020-11-09 22:35:52 +0000

    codingtrain recursive, create single branch

--- src/index.js
+++ src/index.js
@@ -17,8 +17,9 @@ import "./main.scss";
 // import "./examples/penrose-tiles.js"
 // import "./examples/fractal-tree.js"
 // import "./moire-patterns/moire.simple.js"
-import "./p5JsFractals/onclick/fractal.instant.onclick.js"
+// import "./p5JsFractals/onclick/fractal.instant.onclick.js"
 // import "./p5JsFractals/generators/fractal.animated.onclick.js"
 // import "./p5JsFractals/p5.org.example.lsystem.js"
 // import "./examples/bouncing-ball/index.js"
 // import "./examples/color-sketch.js";
+import "./thecodingtrain/recursiveFractalTree.js"
---
+++ src/thecodingtrain/recursiveFractalTree.js
@@ -0,0 +1,28 @@
+// go to /node_modules and get p5.js
+import p5ctx from 'p5';
+// create a new p5 instance. docs - https://p5js.org/reference/
+const p5 =  new p5ctx(
+  // now we can invoke p5 object|class|instance methods
+  (p5) => {
+    // all the processing goes here...
+    p5.setup = ()=>{
+      // only invoked once
+      p5.createCanvas(400, 400)
+      // color (rgb)
+      p5.background(51)      
+    }
+    // 
+    p5.draw = ()=>{
+      // frameRate || deltaTime
+      const len = 100;
+      p5.stroke(255);
+      p5.line(200, p5.height, 200, p5.height -len);
+    }
+  }
+)
+
+
+
+
+
+export default p5
