029acd3b655a0e8e8f228e43f876057035ae15ec
Cloudbop <boppincolin@gmail.com>
2021-01-10 00:43:45 +0000

    added leaves, jitter, animation

--- src/oop-fractal-tree/branch.js
+++ src/oop-fractal-tree/branch.js
@@ -1,19 +1,26 @@
+// TODO set option up
+
 // constructor
 import p5 from 'p5'
 function Branch(begin, end){
   this.begin = begin
   this.end = end
+  this.finished=false;
   //
   this.show = function(p) {
     p.stroke(255)
     p.line(this.begin.x, this.begin.y, this.end.x, this.end.y)
   }
 
+  this.randomRotate = function(p) {
+    return p.random(2,8);
+  }
+
   this.branchRight = function(p) {
     // vector
     const dir= p5.Vector.sub(this.end, this.begin)
     // 45deg
-    dir.rotate(p.PI/4)
+    dir.rotate(this.randomRotate(p))
     // shrink
     dir.mult(0.67)
     const newEnd = p5.Vector.add(this.end, dir);
@@ -30,6 +37,11 @@ function Branch(begin, end){
     const newEnd = p5.Vector.add(this.end, dir);
 
     return new Branch(this.end, newEnd)
+  }
+
+  this.jitter = function(p) {
+    this.end.x += p.random(-1,1)
+    this.end.y += p.random(-1,1) 
   }
 }
 
--- src/oop-fractal-tree/oop.fractal.js
+++ src/oop-fractal-tree/oop.fractal.js
@@ -5,41 +5,73 @@ import Branch from './branch';
 const p5 =  new p5ctx(
   // now we can invoke p5 object|class|instance methods
   (p5) => {
-    // all the processing goes here...
+    // arrays of Vectors
     const tree=[];
+    const leaves=[];
     let root;
+    let countGeneration=0
     
     p5.setup = ()=>{
       p5.createCanvas(window.innerWidth, window.innerHeight)
-      p5.background(90,20,10)
-      //
+      p5.background(190,220,10)
+      // starting point
       const a = p5.createVector(p5.width/2, p5.height);
       const b = p5.createVector(p5.width/2, p5.height-100);
       root = new Branch(a,b, p5);
-
+      // array of branch objects
       tree[0] = root
     }
     // 
     p5.draw = ()=>{
-      p5.background(90,20,10)
+      // p5.background(90,20,10)
+      p5.background(0,0,0)
 
       for (let idx = 0; idx < tree.length; idx++) {
         tree[idx].show(p5);
+        // animate!
+        // tree[idx].jitter(p5);
+      }
+
+      for (let i = 0; i < leaves.length; i++) {
+        const leaf = leaves[i];
+        // rgba
+        p5.fill(p5.random(0,255) ,p5.random(0,255),p5.random(0,255),100)
+        p5.noStroke()
+        p5.ellipse(leaf.x, leaf.y, 18, 18)
+        
+        // falling leaves!!!
+        leaf.y+= p5.random(0,1)
       }
     }
+
+    /**
+     * Pre Bug Fix explaination - .finished
+     * each click is rendering brand new tree over previous one
+     */
 
 
     p5.mousePressed = ()=> {
       for (let idx = tree.length-1; idx>=0; idx--) {
-        tree.push(tree[idx].branchRight(p5));
-        tree.push(tree[idx].branchLeft(p5));        
-        // tree[1] = tree[0].branchRight(p5)
-        // tree[2] = tree[0].branchLeft(p5)
+        
+        if(!tree[idx].finished){
+          tree.push(tree[idx].branchRight(p5));
+          tree.push(tree[idx].branchLeft(p5));          
+        }
+        tree[idx].finished = true;
       }
-      // 
-      
-    }
+      //
+      countGeneration++
 
+      if(countGeneration%2===0){
+        for (let i = 0; i < tree.length; i++) {     
+          if(!tree[i].finished){
+            // copy the vector
+            const leaf = tree[i].end.copy()
+            leaves.push(leaf)
+          }
+        }
+      }
+    }
   }
 )
 
